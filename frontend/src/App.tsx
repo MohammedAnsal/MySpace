66c432e96dfd91f5d9c61810fa9131d93758e9d0
MohammedAnsal <ansalshaah786@gmail.com>
2025-02-05 19:04:33 +0530

    add:frontend-setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="">
+      <p className="text-xl text-green-400 text-center pt-32">Welcome Back</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+
+const Welcome = () => {
+  return (
+    <div>
+      <p className='text-red-800'>Welcomeeeee</p>
+    </div>
+  )
+}
+
+export default Welcome
