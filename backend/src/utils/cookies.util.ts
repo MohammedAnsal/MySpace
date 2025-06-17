6bf7ff0e18204b7279db39202c3c7e7437ebde23
MohammedAnsal <ansalshaah786@gmail.com>
2025-06-18 04:18:39 +0530

    fix:- cookie problem

--- backend/src/utils/cookies.util.ts
+++ backend/src/utils/cookies.util.ts
@@ -4,8 +4,15 @@ export const setCookie = (res: Response, type: string, token: string) => {
 
   res.cookie(type, token, {
     httpOnly: true,
-    secure:true,
-    sameSite: "strict",
+    secure: false, // true only in production
+    sameSite: "lax", // allow cookies in cross-origin dev
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
   });
+
+  // res.cookie(type, token, {
+  //   httpOnly: true,
+  //   secure:true,
+  //   sameSite: "strict",
+  //   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  // });
 };
