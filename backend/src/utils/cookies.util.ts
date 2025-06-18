444954b26a809b1c5aab79b9cab5c45917781cfd
MohammedAnsal <ansalshaah786@gmail.com>
2025-06-18 10:16:16 +0530

    fix:- coockie

--- backend/src/utils/cookies.util.ts
+++ backend/src/utils/cookies.util.ts
@@ -4,8 +4,8 @@ export const setCookie = (res: Response, type: string, token: string) => {
 
   res.cookie(type, token, {
     httpOnly: true,
-    secure: false, // true only in production
-    sameSite: "lax", // allow cookies in cross-origin dev
+    secure: true, // true only in production
+    sameSite: 'none', // allow cookies in cross-origin dev
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
   });
 
