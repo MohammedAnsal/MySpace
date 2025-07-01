8d73e4c6213a0c61e7476bdb20c440cf6d594ed7
MohammedAnsal <ansalshaah786@gmail.com>
2025-07-01 12:55:00 +0530

    fix: handled bug in image sender while uploading or processing ✅

--- frontend/src/services/Api/notificationApi.ts
+++ frontend/src/services/Api/notificationApi.ts
@@ -1,6 +1,6 @@
 import { userAxiosInstance } from "../axiosInstance/userInstance";
 
-const API_BASE_URL = "https://api.my-space.shop/notification";
+const API_BASE_URL = "http://localhost:7001/notification";
 
 export const createNotification = async (data: any) => {
   try {
